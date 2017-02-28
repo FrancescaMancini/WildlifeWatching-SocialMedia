3fd23999b16e379f8cee3d1bb869443c2c2ec565
Francesca Mancini <r03fm14@abdn.ac.uk>
2017-02-28 11:50:59 +0000

    New script for collinearity function and modify code in Analysis

---
+++ Collinearity.R
@@ -0,0 +1,63 @@
+########################################################################################
+#Collinearity functions
+#Written by Gudrun Carl, 2005-2007 
+#and available at http://www.ecography.org/appendix/e5171
+#Dormann, F. C., McPherson, J. M., Araújo, M. B., Bivand, R., Bolliger, J., 
+#Carl, G., Davies, R. G., Hirzel, A., Jetz, W., Kissling, W. D., Kühn, I., 
+#Ohlemüller, R., Peres-Neto, P. R., Reineking, B., Schröder, B., Schurr, F. M. 
+#and Wilson, R. 2007. Methods to account for spatial autocorrelation in the analysis 
+#of species distributional data: a review. – Ecography 30: 609–628.
+########################################################################################
+
+#function to calculate VIF for the predictors
+corvif <- function(dataz) {
+  dataz <- as.data.frame(dataz)
+  #correlation part
+  cat("Correlations of the variables\n\n")
+  tmp_cor <- cor(dataz,use="complete.obs")
+  print(tmp_cor)
+  
+  #vif part
+  form    <- formula(paste("fooy ~ ",paste(strsplit(names(dataz)," "),collapse=" + ")))
+  dataz   <- data.frame(fooy=1,dataz)
+  lm_mod  <- lm(form,dataz)
+  
+  cat("\n\nVariance inflation factors\n\n")
+  print(myvif(lm_mod))
+}
+
+myvif <- function(mod) {
+  v <- vcov(mod)
+  assign <- attributes(model.matrix(mod))$assign
+  if (names(coefficients(mod)[1]) == "(Intercept)") {
+    v <- v[-1, -1]
+    assign <- assign[-1]
+  } else warning("No intercept: vifs may not be sensible.")
+  terms <- labels(terms(mod))
+  n.terms <- length(terms)
+  if (n.terms < 2) stop("The model contains fewer than 2 terms")
+  if (length(assign) > dim(v)[1] ) {
+    diag(tmp_cor)<-0
+    if (any(tmp_cor==1.0)){
+      return("Sample size is too small, 100% collinearity is present")
+    } else {
+      return("Sample size is too small")
+    }
+  }
+  R <- cov2cor(v)
+  detR <- det(R)
+  result <- matrix(0, n.terms, 3)
+  rownames(result) <- terms
+  colnames(result) <- c("GVIF", "Df", "GVIF^(1/2Df)")
+  for (term in 1:n.terms) {
+    subs <- which(assign == term)
+    result[term, 1] <- det(as.matrix(R[subs, subs])) * det(as.matrix(R[-subs, -subs])) / detR
+    result[term, 2] <- length(subs)
+  }
+  if (all(result[, 2] == 1)) {
+    result <- data.frame(GVIF=result[, 1])
+  } else {
+    result[, 3] <- result[, 1]^(1/(2 * result[, 2]))
+  }
+  invisible(result)
+}
